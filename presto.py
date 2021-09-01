d9bd43238b947f8423a6dd0255f44a5a57c1fd54
lizongti <lzongti@gmail.com>
2021-09-01 18:42:09 +0800

    init

---
+++ presto.py
@@ -0,0 +1,29 @@
+import redis
+import json
+import os
+
+host = "localhost"
+port = 51002
+password = "adminpass"
+db = 0
+
+
+class TableRowImporter:
+    def __init__(self, schema, table):
+        self.schema = schema
+        self.table = table
+        self.conn = redis.Redis(
+            host=host, port=port, password=password, db=db)
+        self.fields = []
+        json_path = "%s\\presto\\tables\\%s.%s.json" % (
+            os.getcwd(), self.schema, self.table)
+        with open(json_path) as f:
+            data = json.loads(f.read())
+            for field in data["value"]["fields"]:
+                self.fields.append(field["name"])
+
+    def save(self, key, values):
+        with self.conn.pipeline(transaction=False) as pipe:
+            name = ":".join([self.schema, self.table, key])
+            for i in range(0, len(self.fields)):
+                self.conn.hset(name, self.fields[i], values[i])
