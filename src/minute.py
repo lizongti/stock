aa5cfb70a90dc8d202c9ce3365e7c0633826ff44
lizongti <lzongti@gmail.com>
2021-09-02 20:21:16 +0800

    update stock.minute

--- src/minute.py
+++ src/minute.py
@@ -21,16 +21,20 @@ def update():
         print(" -> Done!")
 
 
-@retry(stop_max_attempt_number=100)
+# @retry(stop_max_attempt_number=100)
 def __update_symbol(importer: presto.TableRowImporter, symbol: str):
     print(".", end='')
     df = ak.stock_zh_a_hist_min_em(symbol=symbol)
     for row in df.iterrows():
-        values = row[1].values
-        minute = values[0]
+        minute = row[1].values[0]
         key = "%s:%s" % (symbol, minute.replace(
             " ", ":").replace("-", ":"))
-        values[0] += ":00.000"
+        mintute_parts = minute.split(" ")
+        date = mintute_parts[0]
+        time = mintute_parts[1]+":00.000"
+
+        values = [symbol, date, time]
+        values.extend(row[1].values[1:])
         importer.save(key, values)
 
 
