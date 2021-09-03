c52255760ca2fda9ed345d27bc9f05815c110f43
lizongti <lzongti@gmail.com>
2021-09-03 21:54:53 +0800

    update data

--- sql/rate_sell.sql
+++ sql/rate_sell.sql
@@ -1,4 +1,4 @@
-select avg(b.opening/a.opening) as rate, b.time from
-(select symbol, date, time, opening from minute) b inner join
-(select symbol, date, opening from (select row_number() over (partition by symbol, date order by time asc) as num, symbol, date, opening from minute) where num = 1) a
+select avg(b.closing/a.closing) as rate, b.time from
+(select symbol, date, time, closing from minute) b inner join
+(select symbol, date, closing from (select row_number() over (partition by symbol, date order by time asc) as num, symbol, date, closing from minute) where num = 1) a
 ON a.symbol = b.symbol and a.date = b.date group by b.time order by rate desc;
