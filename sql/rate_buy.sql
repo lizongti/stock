6c43964a2d4c971ceb158c83afe1a689f077cf34
lizongti <503622409@qq.com>
2021-09-04 22:36:26 +0800

    update

--- sql/rate_buy.sql
+++ sql/rate_buy.sql
@@ -1,10 +1,4 @@
 select avg(b.closing/a.closing) as rate, b.time from
 (select symbol, date, time, closing from minute) b inner join
-(
-    select
-(select a1.symbol, a1.date, case when a2.closing > a1.closing then 1 else -1 end as state, a2.closing from
-(select symbol, date, closing from (select row_number() over (partition by symbol, date order by time asc) as num, symbol, date, closing from minute) where num = 1) a1,
-(select symbol, date, closing from (select row_number() over (partition by symbol, date order by time desc) as num, symbol, date, closing from minute) where num = 1) a2
-where a1.symbol = a2.symbol and a1.date = a2.date)
-) a
+(select symbol, date, closing from (select row_number() over (partition by symbol, date order by time desc) as num, symbol, date, closing from minute) where num = 1) a
 ON a.symbol = b.symbol and a.date = b.date group by b.time order by rate asc;
