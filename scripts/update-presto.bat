fc41cc77fbb1667eeebe256574c3a68b7b206204
lizongti <lzongti@gmail.com>
2021-09-07 20:13:49 +0800

    fix config

--- scripts/update-presto.bat
+++ scripts/update-presto.bat
@@ -2,9 +2,6 @@ if not exist "scripts" (cd ..)
 
 docker run --rm ^
 --name presto-updater ^
--v presto_conf:/opt/presto_conf ^
--v %cd%/presto:/opt/presto ^
-centos:latest bash -c "cp -rf /opt/presto/* /opt/presto_conf"
-
-docker restart presto
-docker logs presto
+-v %cd%/presto-conf:/opt/presto-conf-src ^
+-v presto-conf:/opt/presto-conf-dst ^
+centos:latest bash -c "cp -rf /opt/presto-conf-src/* /opt/presto-conf-dst"
