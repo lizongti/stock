92a9daadb89dea494de62569c2ad0669672ac5b7
lizongti <lzongti@gmail.com>
2021-09-07 20:26:09 +0800

    update presto-coordinator

--- scripts/update-presto.bat
+++ scripts/update-presto.bat
@@ -3,5 +3,7 @@ if not exist "scripts" (cd ..)
 docker run --rm ^
 --name presto-updater ^
 -v %cd%/presto-conf:/opt/presto-conf-src ^
--v presto-conf:/opt/presto-conf-dst ^
+-v hive_presto-conf:/opt/presto-conf-dst ^
 centos:latest bash -c "cp -rf /opt/presto-conf-src/* /opt/presto-conf-dst"
+
+docker service update hive_presto-coordinator
