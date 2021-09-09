d46e2dd20fb433d0793e41641e3c56427ffbc7b2
lizongti <lzongti@gmail.com>
2021-09-09 20:13:53 +0800

    update

--- scripts/update-presto.bat
+++ scripts/update-presto.bat
@@ -4,6 +4,6 @@ docker run --rm ^
 --name presto-updater ^
 -v %cd%/presto:/src/presto ^
 -v hive_presto:/dst/presto ^
-centos:latest bash -c "cp -rf /src/presto/* /dst/presto/"
+centos:latest bash -c "rm -rf /dst/presto/* && cp -rf /src/presto/* /dst/presto/"
 
 docker service update hive_presto --force
