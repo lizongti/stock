a2eb15c9cb68eed286ea148e65d8414cfd17ad96
lizongti <lzongti@gmail.com>
2021-09-10 12:09:11 +0800

    update

--- docker/init/pika.bat
+++ docker/init/pika.bat
@@ -1,8 +1,9 @@
-if not exist "scripts" (cd ..)
+if not exist "docker" (cd ..)
+if not exist "docker" (cd ..)
 
 docker run --rm ^
 --name pika-updater ^
--v %cd%/pika:/src ^
+-v %cd%/docker/init/pika:/src ^
 -v hive_pika-conf:/dst ^
 centos:latest bash -c "rm -rf /dst/* && cp -rf /src/* /dst/"
 
--- docker/init/presto.bat
+++ docker/init/presto.bat
@@ -1,8 +1,9 @@
-if not exist "scripts" (cd ..)
+if not exist "docker" (cd ..)
+if not exist "docker" (cd ..)
 
 docker run --rm ^
 --name presto-updater ^
--v %cd%/presto:/src ^
+-v %cd%/docker/init/presto:/src ^
 -v hive_presto:/dst ^
 centos:latest bash -c "rm -rf /dst/* && cp -rf /src/* /dst/"
 
