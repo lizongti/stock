c0f74fa67629fc27810c1c9de8a537e76e70a495
lizongti <lzongti@gmail.com>
2021-09-14 15:11:20 +0800

    rebuild

--- docker/build/build-scheduler.bat
+++ docker/build/build-scheduler.bat
@@ -1,5 +1,4 @@
 if not exist "docker" (cd ..)
 if not exist "docker" (cd ..)
-pipreqs ./src --encoding=utf-8 --force
 docker build -t lizongti/stock:scheduler -f ./src/scheduler.dockerfile ./src
 docker push lizongti/stock:scheduler 
