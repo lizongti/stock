9f7ae66af7d6dc324a0e3f9e4d66950fdda9b1c2
lizongti <lzongti@gmail.com>
2021-09-14 15:41:27 +0800

    update stack

--- docker/build/build-scheduler.bat
+++ docker/build/build-scheduler.bat
@@ -1,4 +1,4 @@
 if not exist "docker" (cd ..)
 if not exist "docker" (cd ..)
-docker build --no-cache -t lizongti/stock:scheduler -f ./src/scheduler.dockerfile ./src
-docker push lizongti/stock:scheduler 
+docker build --no-cache -t lizongti/stock:latest -f ./src/scheduler.dockerfile ./src
+docker push lizongti/stock:latest 
