5ef2d54e64d36acf3b7c47f2675d46c33cf3afc6
lizongti <lzongti@gmail.com>
2021-09-28 15:14:10 +0800

    update

--- sql/query/price.sql
+++ sql/query/price.sql
@@ -1,37 +1,37 @@
 # higheast
 select count(*) as count, time from
-(select row_number() over (partition by code, date order by higheast desc) as n, time from hive.stock.minutes where date>'2015-01-01')
+(select row_number() over (partition by code, date order by higheast desc) as n, time from hive.stock.minutes)
 where n = 1 group by time order by count desc;
 
 # loweast
 select count(*) as count, time from
-(select row_number() over (partition by code, date order by loweast asc) as n, time from hive.stock.minutes where date>'2015-01-01')
+(select row_number() over (partition by code, date order by loweast asc) as n, time from hive.stock.minutes)
 where n = 1 group by time order by count desc;
 
 # up higheast
 select count(*) as count, time from
-(select code, date, time from (select code, date, row_number() over (partition by code, date order by higheast desc) as n, time from hive.stock.minutes where date>'2015-01-01') where n = 1) minutes,
-(select code, date from postgresql.stock.days where closing > opening and date>'2015-01-01') days
+(select code, date, time from (select code, date, row_number() over (partition by code, date order by higheast desc) as n, time from hive.stock.minutes) where n = 1) minutes,
+(select code, date from postgresql.stock.days where closing > opening) days
 where minutes.date = days.date and minutes.code = days.code
 group by minutes.time order by count desc;
 
 # up loweast
 select count(*) as count, time from
-(select code, date, time from (select code, date, row_number() over (partition by code, date order by higheast asc) as n, time from hive.stock.minutes where date>'2015-01-01') where n = 1) minutes,
-(select code, date from postgresql.stock.days where closing > opening and date>'2015-01-01') days
+(select code, date, time from (select code, date, row_number() over (partition by code, date order by higheast asc) as n, time from hive.stock.minutes) where n = 1) minutes,
+(select code, date from postgresql.stock.days where closing > opening) days
 where minutes.date = days.date and minutes.code = days.code
 group by minutes.time order by count desc;
 
 # down higheast
 select count(*) as count, time from
-(select code, date, time from (select code, date, row_number() over (partition by code, date order by higheast desc) as n, time from hive.stock.minutes where date>'2015-01-01') where n = 1) minutes,
-(select code, date from postgresql.stock.days where closing < opening and date>'2015-01-01') days
+(select code, date, time from (select code, date, row_number() over (partition by code, date order by higheast desc) as n, time from hive.stock.minutes) where n = 1) minutes,
+(select code, date from postgresql.stock.days where closing < opening) days
 where minutes.date = days.date and minutes.code = days.code
 group by minutes.time order by count desc;
 
 # down loweast
 select count(*) as count, time from
-(select code, date, time from (select code, date, row_number() over (partition by code, date order by higheast asc) as n, time from hive.stock.minutes where date>'2015-01-01') where n = 1) minutes,
-(select code, date from postgresql.stock.days where closing < opening and date>'2015-01-01') days
+(select code, date, time from (select code, date, row_number() over (partition by code, date order by higheast asc) as n, time from hive.stock.minutes) where n = 1) minutes,
+(select code, date from postgresql.stock.days where closing < opening) days
 where minutes.date = days.date and minutes.code = days.code
 group by minutes.time order by count desc;
