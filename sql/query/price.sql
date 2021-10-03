b4d9863ec724bf3cff819b03870ed5df1531465d
lizongti <lzongti@gmail.com>
2021-10-03 22:56:36 +0800

    fix

--- sql/query/price.sql
+++ sql/query/price.sql
@@ -3,9 +3,9 @@ select count(*) as count, time from
 (select row_number() over (partition by code, date order by high desc) as n, time from hive.stock.minutes)
 where n = 1 group by time order by count desc;
 
-# loweast
+# low
 select count(*) as count, time from
-(select row_number() over (partition by code, date order by loweast asc) as n, time from hive.stock.minutes)
+(select row_number() over (partition by code, date order by low asc) as n, time from hive.stock.minutes)
 where n = 1 group by time order by count desc;
 
 # up high
@@ -15,9 +15,9 @@ select count(*) as count, time from
 where minutes.date = days.date and minutes.code = days.code
 group by minutes.time order by count desc;
 
-# up loweast
+# up low
 select count(*) as count, time from
-(select code, date, time from (select code, date, row_number() over (partition by code, date order by high asc) as n, time from hive.stock.minutes) where n = 1) minutes,
+(select code, date, time from (select code, date, row_number() over (partition by code, date order by low asc) as n, time from hive.stock.minutes) where n = 1) minutes,
 (select code, date from postgresql.stock.days where close > open) days
 where minutes.date = days.date and minutes.code = days.code
 group by minutes.time order by count desc;
@@ -29,9 +29,9 @@ select count(*) as count, time from
 where minutes.date = days.date and minutes.code = days.code
 group by minutes.time order by count desc;
 
-# down loweast
+# down low
 select count(*) as count, time from
-(select code, date, time from (select code, date, row_number() over (partition by code, date order by high asc) as n, time from hive.stock.minutes) where n = 1) minutes,
+(select code, date, time from (select code, date, row_number() over (partition by code, date order by low asc) as n, time from hive.stock.minutes) where n = 1) minutes,
 (select code, date from postgresql.stock.days where close < open) days
 where minutes.date = days.date and minutes.code = days.code
 group by minutes.time order by count desc;
