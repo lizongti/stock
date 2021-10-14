8804cceea9ee612153961652f5de9d4c3eb6cdf4
lizongti <lzongti@gmail.com>
2021-10-14 10:06:08 +0800

    fix

--- src/scheduler.py
+++ src/scheduler.py
@@ -22,8 +22,8 @@ def _run(date):
 
 
 def _level0(date):
-    # StocksController().run()
-    # DaysController().run(date)
+    StocksController().run()
+    DaysController().run(date)
     MinutesPartitialController().run(date)
     MinutesController().run(date)
     IndicatorController().run(date)
